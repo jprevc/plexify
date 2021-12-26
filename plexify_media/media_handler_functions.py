b604aca2c8cb670f73fe7e1667786cc9267993f8
jprevc <jost.prevc@gmail.com>
2021-12-27 00:05:23 +0100

    - media handlers are now classes, which all implement the same interface, added function which will return appropriate handler according to label
    - added 'subtitles' argument, where user can specify languages of subtitles to be found and downloaded

--- plexify_media/media_handler_functions.py
+++ plexify_media/media_handler_functions.py
@@ -4,120 +4,156 @@ Contains various media handler functions.
 
 import re
 import os
+import sys
+import inspect
 from distutils.dir_util import copy_tree
 from shutil import copyfile
 from datetime import timedelta
+from abc import ABC, abstractmethod
+from typing import List
 
 from babelfish import Language
 from subliminal import download_best_subtitles, save_subtitles, scan_videos
 
 
-def movie_handler(cli_args):
+def label_to_media_handler_map():
+    module_classes = list(zip(*inspect.getmembers(sys.modules[__name__], inspect.isclass)))[1]
+    media_handler_classes = list(filter(lambda x: issubclass(x, MediaHandlerBase), module_classes))
+
+    return {mh_class.label: mh_class for mh_class in media_handler_classes}
+
+
+class MediaHandlerBase(ABC):
+    """
+    Base class for any media handler.
+    """
+
+    def __init__(self, cli_args):
+        self.cli_args = cli_args
+
+    def run(self):
+        output_folder_location = self.get_output_folder_location()
+
+        # copy media files to plex
+        self.copy_torrent_to_plex(output_folder_location)
+
+        self.post_processing_hook(output_folder_location)
+
+    @abstractmethod
+    def get_output_folder_location(self) -> str:
+        pass
+
+    def post_processing_hook(self, output_folder_path: str):
+        pass
+
+    def copy_torrent_to_plex(self, output_folder_path: str):
+        """
+        Copies downloaded torrent to plex location.
+        """
+        if self.cli_args.torrent_kind == "multi":
+            copy_tree(self.cli_args.download_location, output_folder_path)
+        elif self.cli_args.torrent_kind == "single":
+            os.mkdir(output_folder_path)
+            copyfile(
+                os.path.join(self.cli_args.download_location, self.cli_args.torrent_name),
+                os.path.join(output_folder_path, self.cli_args.torrent_name),
+            )
+        else:
+            raise ValueError(f"torrent_kind should either be 'single' or 'multi' and not {self.cli_args.torrent_kind}")
+
+    @classmethod
+    @property
+    @abstractmethod
+    def label(cls):
+        pass
+
+
+class MovieHandler(MediaHandlerBase):
     """
     Handler for movie files.
     """
 
-    def get_movie_name_and_year(torrent_name):
+    @classmethod
+    @property
+    @abstractmethod
+    def label(cls):
+        return 'movie'
+
+    def get_output_folder_location(self) -> str:
+        # parse movie name and year from torrent name
+        movie_name, movie_year = self._get_movie_name_and_year(self.cli_args.torrent_name)
+
+        # create output folder names and path to which movie will be copied
+        output_movie_folder_name = f"{movie_name} ({movie_year})"
+        output_movie_folder_path = os.path.join(self.cli_args.plex_location, "movies", output_movie_folder_name)
+
+        return output_movie_folder_path
+
+    def post_processing_hook(self, output_folder_path: str):
+        download_subtitles(output_folder_path, self.cli_args.subtitles)
+
+    @staticmethod
+    def _get_movie_name_and_year(torrent_name: str):
         match = re.match(r"(.*?)(\d{4})", torrent_name)
         name = match[1].replace(".", " ").replace("-", "").replace("_", "").strip()
         year = match[2]
 
         return name, year
 
-    # parse movie name and year from torrent name
-    movie_name, movie_year = get_movie_name_and_year(cli_args.torrent_name)
 
-    # create output folder names and path to which movie will be copied
-    output_movie_folder_name = f"{movie_name} ({movie_year})"
-    output_movie_folder_path = os.path.join(cli_args.plex_location, "movies", output_movie_folder_name)
+class ShowHandler(MediaHandlerBase):
+    @classmethod
+    @property
+    @abstractmethod
+    def label(cls):
+        return 'show'
 
-    # copy movie to plex
-    copy_torrent_to_plex(
-        cli_args.torrent_name, cli_args.torrent_kind, cli_args.download_location, output_movie_folder_path
-    )
+    def get_output_folder_location(self) -> str:
+        # parse show name and season number from torrent name
+        show_name, season = self._get_show_name_and_season(self.cli_args.torrent_name)
 
-    # try to find subtitles if it was specified in label
-    if "sub" in cli_args.label.split("_"):
-        download_subtitles(output_movie_folder_path)
+        # create output folder names and path to which show videos will be copied
+        output_show_folder_path = os.path.join(self.cli_args.plex_location, "shows", show_name, "Season " + season)
 
+        return output_show_folder_path
 
-def show_handler(cli_args):
-    """
-    Handler for shows.
-    """
+    def post_processing_hook(self, output_folder_path: str):
+        download_subtitles(output_folder_path, self.cli_args.subtitles)
 
-    def get_show_name_and_season(download_folder):
+    @staticmethod
+    def _get_show_name_and_season(download_folder: str):
         match = re.match(r"(.*)[S|s](\d{1,2})", download_folder)
         name = match[1].replace(".", " ").replace("-", " ").replace("_", " ").strip()
         season = match[2]
 
         return name, season
 
-    # parse show name and season number from torrent name
-    show_name, season = get_show_name_and_season(cli_args.torrent_name)
 
-    # create output folder names and path to which show videos will be copied
-    output_show_folder_path = os.path.join(cli_args.plex_location, "shows", show_name, "Season " + season)
-
-    # copy torrent to plex
-    copy_torrent_to_plex(
-        cli_args.torrent_name, cli_args.torrent_kind, cli_args.download_location, output_show_folder_path
-    )
-
-    # try to find subtitles if it was specified in label
-    if "sub" in cli_args.label.split("_"):
-        download_subtitles(output_show_folder_path)
-
-
-def video_handler(cli_args):
-    """
-    Handler for videos
-    """
-    default_handler(cli_args, "video")
-
-
-def music_handler(cli_args):
-    """
-    Handler for music files.
-    """
-    default_handler(cli_args, "music")
-
-
-def image_handler(cli_args):
-    """
-    Handler for images.
-    """
-    default_handler(cli_args, "photos")
-
-
-def default_handler(cli_args, output_folder_name):
+class DefaultHandler(MediaHandlerBase):
     """
     Default media handler.
     """
-    output_folder_path = os.path.join(cli_args.plex_location, output_folder_name, cli_args.torrent_name)
-    copy_torrent_to_plex(cli_args.torrent_name, cli_args.torrent_kind, cli_args.download_location, output_folder_path)
+
+    @classmethod
+    @property
+    @abstractmethod
+    def label(cls):
+        return 'default'
+
+    def get_output_folder_location(self) -> str:
+        return os.path.join(self.cli_args.plex_location, self.cli_args.label, self.cli_args.torrent_name)
 
 
-def download_subtitles(video_folder):
-    """Downloads best found slovenian and english subtitles for every video file in video folder"""
-
+def download_subtitles(video_folder: str, languages: List[str]):
+    """
+    Downloads best found slovenian and english subtitles for every video file in video folder.
+    """
     # scan video folder for video files
     videos = scan_videos(video_folder, age=timedelta(days=2))
 
-    # download english and slovenian subtitles for all found videos
-    subtitles = download_best_subtitles(videos, {Language("eng"), Language("slv")})
+    # download specified subtitles for all found videos
+    subtitles = download_best_subtitles(set(videos), {Language(lang_code) for lang_code in languages})
 
     # save subtitles next to the videos
     for video in videos:
         save_subtitles(video, subtitles[video])
-
-
-def copy_torrent_to_plex(torrent_name, torrent_kind, download_location, output_folder_path):
-    """Copies downloaded torrent to plex location"""
-    if torrent_kind == "multi":
-        copy_tree(download_location, output_folder_path)
-    elif torrent_kind == "single":
-        os.mkdir(output_folder_path)
-        copyfile(os.path.join(download_location, torrent_name), os.path.join(output_folder_path, torrent_name))
-    else:
-        raise ValueError(f"torrent_kind should either be 'single' or 'multi' and not {torrent_kind}")
--- plexify_media/plexify.py
+++ plexify_media/plexify.py
@@ -20,6 +20,8 @@ def main():
     parser.add_argument("--label", help="label given to downloaded media")
     parser.add_argument("--download_location", help="location of downloaded media")
     parser.add_argument("--plex_location", help="location of plex media storage")
+    parser.add_argument("--subtitles", action="append", required=False,
+                        help="If specified, program will attempt to find subtitles of the specified language code.")
 
     args = parser.parse_args()
 
@@ -33,23 +35,9 @@ def main():
     if not all([args.label, args.download_location, args.plex_location]):
         sys.exit(0)
 
-    # entries in label should be split by _ (ex. movie_sub)
-    label_lst = args.label.split("_")
-
-    media_handler_functions = {
-        "movie": mhf.movie_handler,
-        "show": mhf.show_handler,
-        "video": mhf.video_handler,
-        "music": mhf.music_handler,
-        "image": mhf.image_handler,
-    }
-
-    if label_lst[0] not in media_handler_functions:
-        raise RuntimeError(f"Label value {label_lst[0]} was not recognised.")
-
     # call appropriate handler according to media label that was given when downloading a torrent
-    media_handler = media_handler_functions[label_lst[0]]
-    media_handler(args)
+    media_handler = mhf.label_to_media_handler_map().get(args.label, mhf.DefaultHandler)
+    media_handler(args).run()
 
 
 if __name__ == "__main__":
