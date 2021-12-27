2075c3a7dc612c59f304aea8053d5a291a796343
jprevc <jost.prevc@gmail.com>
2021-12-27 13:16:27 +0100

    major version incremented

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="PlexifyMedia",
-    version="1.1",
+    version="2.0",
     description="Copy and arrange media files to Plex home server",
     author="jprevc",
     author_email="jost.prevc@gmail.com",
