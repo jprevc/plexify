00a48dec6d669867bfe1f16f2bebdfe7a48f6752
jprevc <jost.prevc@gmail.com>
2021-12-27 13:17:04 +0100

    added hotfix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="PlexifyMedia",
-    version="2.0",
+    version="2.0.0",
     description="Copy and arrange media files to Plex home server",
     author="jprevc",
     author_email="jost.prevc@gmail.com",
